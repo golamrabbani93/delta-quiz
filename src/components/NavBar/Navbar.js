bfdac151bb0058ac86cdf68ca341b34d032db4d8
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-10 22:57:04 +0600

    complete Navbar components

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
 	return (
-		<div>
-			<h2>this is navigation</h2>
-		</div>
+		<header className="md:flex justify-between items-center px-20 py-3 text-white">
+			<div className="logo text-4xl ">
+				<Link to="/">DeltaQuiz</Link>
+			</div>
+			<nav className="pr-24">
+				<NavLink to="/" end>
+					Topic
+				</NavLink>
+				<NavLink to="/statics">Statics</NavLink>
+			</nav>
+		</header>
 	);
 };
 
