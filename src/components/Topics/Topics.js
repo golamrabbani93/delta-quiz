dc11f04d08f7b679406f1a390812fb56a4be2c9f
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-10 23:21:10 +0600

    create loader at topics route

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useLoaderData} from 'react-router-dom';
 
 const Topics = () => {
+	const quiz = useLoaderData();
+	console.log(quiz);
 	return (
 		<div>
 			<h2>This topics component</h2>
