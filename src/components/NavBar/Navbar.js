b8fec9a44ede5d1d4cc7548b4cbbd002318c3ff2
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-11 00:59:13 +0600

    create practice dynamic link

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
 				</div>
 				<nav className="pr-24">
 					<NavLink to="/" end>
-						Topic
+						Topics
 					</NavLink>
 					<NavLink to="/statics">Statics</NavLink>
 					<NavLink to="/blog">Blog</NavLink>
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Topic.css';
 
 const Topic = ({data}) => {
@@ -7,13 +8,15 @@ const Topic = ({data}) => {
 	return (
 		<div className="topic">
 			<div className="img">
-				<img src={logo} alt={name} />
+				<img src={logo} alt={name} className="p-3" />
 			</div>
-			<h3 className="font-bold pt-2 text-center">Total Quiz Question : {total}</h3>
+			<h3 className="font-bold pt-2 text-center">
+				Total {name} Question : {total}
+			</h3>
 			<div className="details flex justify-around mt-5 items-center ">
 				<h3>{name}</h3>
 				<button>
-					<span>Strat Practice</span>
+					<Link to={`/quiz/${id}`}>Strat Practice</Link>
 				</button>
 			</div>
 		</div>
