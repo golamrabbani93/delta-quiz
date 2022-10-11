447b2435c867f0b550caf9a5189a63aa59d9da88
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-12 00:03:17 +0600

    chart display and responsive complete

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import {useLoaderData} from 'react-router-dom';
-
+import {
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	Bar,
+	BarChart,
+	ResponsiveContainer,
+} from 'recharts';
+import './Statics.css';
 const Statics = () => {
 	const {data} = useLoaderData();
-	console.log('🚀 ~ file: Statics.js ~ line 6 ~ Statics ~ data', data);
+	console.log(data);
+
 	return (
-		<div>
-			<h2>this Statics page</h2>
+		<div className="m-auto chart-container">
+			<h2 className="text-center text-3xl md:text-5xl m-4  md:m-8">Total Quiz List</h2>
+			<ResponsiveContainer width="100%" height={400}>
+				<BarChart className="mx-auto chart" width={530} height={400} data={data}>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="name" />
+					<YAxis />
+					<Tooltip />
+					<Legend />
+					<Bar dataKey="total" fill="#dc164a" />
+				</BarChart>
+			</ResponsiveContainer>
 		</div>
 	);
 };
