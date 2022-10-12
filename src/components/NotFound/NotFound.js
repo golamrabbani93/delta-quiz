9623e0767bf9fb6667096941997688f05213ef0d
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-12 15:07:39 +0600

    not found page responsive complete

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,9 +6,9 @@ const NotFound = () => {
 	return (
 		<div>
 			<Navbar></Navbar>
-			<div className="not-found flex justify-center items-center flex-col">
-				<h2 className="text-7xl mb-3">OPPS!</h2>
-				<h2 className="text-7xl">404 Not Found</h2>
+			<div className="not-found flex justify-center items-center flex-col mt-14">
+				<h2 className="text-4xl md:text-7xl mb-3">OPPS!</h2>
+				<h2 className="text-4xl md:text-7xl">404 Not Found</h2>
 				<Link className="mt-7" to="/">
 					Go To Homepage
 				</Link>
