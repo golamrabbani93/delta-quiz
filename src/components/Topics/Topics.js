fad27a73a7c5ed6412fe4749710aed0db92e3575
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-12 11:56:55 +0600

    change banner height

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useLoaderData} from 'react-router-dom';
 import Topic from '../Topic/Topic';
+import './Topics.css';
 
 const Topics = () => {
 	const {data} = useLoaderData();
