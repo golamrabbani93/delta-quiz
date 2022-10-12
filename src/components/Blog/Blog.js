f3aa9ff0390ef562fe1524ae85b222337724464b
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-12 10:09:07 +0600

    question display complete

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useLoaderData} from 'react-router-dom';
+import SingleBlog from '../SingleBlog/SingleBlog';
 
 const Blog = () => {
 	const questions = useLoaderData();
@@ -9,6 +10,9 @@ const Blog = () => {
 			<h2 className="text-3xl md:text-5xl text-center my-4 md:my-7 ">
 				All {questions.length} Questions
 			</h2>
+			{questions.map((qt) => (
+				<SingleBlog qt={qt} key={qt.id}></SingleBlog>
+			))}
 		</div>
 	);
 };
---
+++ src/components/SingleBlog/SingleBlog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './SingleBlog.css';
+
+const SingleBlog = ({qt}) => {
+	const {id, question, answer} = qt;
+	console.log(id);
+	return (
+		<div className="Single-blog mx-3 md:w-4/6 md:mx-auto border-2">
+			<h2 className="text-1xl md:text-4xl mb-5">
+				<span className="mark">Question :</span> {question}
+			</h2>
+			<h2 className="text-1xl md:text-2xl">
+				<span className="mark">Answer : </span>
+				{answer}
+			</h2>
+		</div>
+	);
+};
+
+export default SingleBlog;
