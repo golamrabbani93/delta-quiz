dae2d086cc258c467d7e25d7685eac22ea830c17
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-12 11:53:18 +0600

    eye icon add and click to show correct answer

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Option from '../Option/Option';
 import './Questions.css';
 
+import {EyeIcon} from '@heroicons/react/24/solid';
 const Questions = ({qt, ind}) => {
+	const [show, showAnswer] = useState(false);
 	ind++;
 	const {question, options, correctAnswer} = qt;
 	return (
 		<div>
-			<div className="question">
+			<div className="question mb-10">
 				<div className="question-details">
 					<h2 className="md:w-4/12 m-auto mt-7 ">
 						Quiz No: {ind} {question}
 					</h2>
-					<h2>hello</h2>
+					<EyeIcon
+						onClick={() => showAnswer(!show)}
+						className="eye h-7 w-7 mx-auto my-3 cursor-pointer"
+					/>
+					<h2 className={`md:w-4/12 m-auto mt-4 ${show ? 'block' : 'hidden'}`}>
+						Correct Answer : {correctAnswer}
+					</h2>
 				</div>
 				<div className="options grid md:grid-cols-2 gap-3 md:w-6/12 m-5 md:m-auto">
 					{options.map((option, ind) => (
